69eb98e62c82118a1c369be0c4ca21f44e2131aa
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-09-18 21:33:27 +0530

    chore: add the solution for valid_anagram_242.py

---
+++ array_and_hashing/valid_anagram_242.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        i = 0
+        sorted_s = "".join(sorted(s))
+        sorted_t = "".join(sorted(t))
+
+        if len(sorted_s) != len(sorted_t):
+            return False
+
+        if sorted_s == sorted_t:
+            return True
+        else:
+            return False
